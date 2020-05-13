eb3478c2d63e9b4e97a628554f0fbeb58ca491a4
limit queen <63425451+LimitQueenProject@users.noreply.github.com>
2020-05-13 20:17:38 +0700

    Add files via upload

---
+++ lim.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+green="\e[1;32m"
+blue="\e[1;34m"
+red="\e[1;31m"
+yellow="\e[0;36"
+clear
+
+menu(){
+echo -e """$blue
+1. Crack Lambat (Dapet banyak akun)
+2. Crack Cepat (Waktu lebih singkat)
+
+"""
+
+
+read -p  "Pilih yang mana : " pil
+
+
+if [[ $pil -eq 1 ]]; then
+sleep 1
+python2 lim-sky.py
+elif [[ $pil -eq 2 ]]; then
+python2 lim-skyv.py
+else
+echo -e "$red pilih yang ada"
+menu
+fi
+}
+menu
